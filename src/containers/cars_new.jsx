b0e00735c17e2a97aa91e07991f0912a28f5f292
Constance Chapman <constancealicechapman224@gmail.com>
2018-09-14 15:51:54 +0100

    form validations

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -4,6 +4,10 @@ import { bindActionCreators } from 'redux';
 import { reduxForm, Field } from 'redux-form';
 import { createCar } from '../actions';
 
+const REQUIRED = value => value ? undefined : 'Required';
+const ALPHANUMERIC = (value) => {
+  return value && /[A-Z0-9]*-[A-Z0-9]*/.test(value) ? undefined : 'Invalid Licence Plate';
+};
 
 class CarsNew extends Component {
   onSubmit = (values) => {
@@ -13,18 +17,19 @@ class CarsNew extends Component {
     });
   }
 
-  renderField(field) {
+  renderField({ input, label, type, meta: { touched, error, warning }}) {
     return (
       <div className="form-group">
-        <label>{field.label}</label>
+        <label>{label}</label>
         <input
           className="form-control"
-          type={field.type}
-          {...field.input}
+          type={type}
+          {...input}
         />
+        {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
       </div>
     );
-  }
+  };
 
   render() {
     return (
@@ -32,31 +37,39 @@ class CarsNew extends Component {
         <div className="car-form">
           <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
             <Field
-              label="Brand"
-              name="brand"
-              type="text"
-              component={this.renderField}
-            />
-            <Field
-              label="Model"
-              name="model"
-              type="text"
-              component={this.renderField}
-            />
-            <Field
-              label="Owner"
-              name="owner"
-              type="text"
-              component={this.renderField}
-            />
-            <Field
-              label="Plate"
-              name="plate"
-              type="text"
-              component={this.renderField}
-            />
+                  label="Brand"
+                  name="brand"
+                  type="text"
+                  validate={[REQUIRED]}
+                  component={this.renderField}
+                  placeholder="Ford"
+                />
+                <Field
+                  label="Model"
+                  name="model"
+                  type="text"
+                  validate={[REQUIRED]}
+                  component={this.renderField}
+                  placeholder="Mustang"
+                />
+                <Field
+                  label="Owner"
+                  name="owner"
+                  type="text"
+                  validate={[REQUIRED]}
+                  component={this.renderField}
+                  placeholder="John Smith"
+                />
+                <Field
+                  label="Plate"
+                  name="plate"
+                  type="text"
+                  validate={[ALPHANUMERIC]}
+                  component={this.renderField}
+                  placeholder="123ABC"
+                />
             <button className="btn btn-primary" type="submit"
-            disabled={this.props.pristine || this.props.submitting}>
+            disabled={this.props.pristine || this.props.invalid}>
               Create Car
             </button>
           </form>
